6cb201de26c881bbd0ace803e622818dff87c1ae
sivakumarkatam <siva.katam@blazeclan.com>
2017-11-30 15:20:03 +0530

    Create testebs.sh

---
+++ testebs.sh
@@ -0,0 +1,23 @@
+sudo su -
+echo "starting ebs volume"
+lsblk
+dh -h
+echo "=========Drive========="
+#Create Mount Directory
+mkdir /data
+#Format the drive
+sudo mkfs -t ext4 /dev/xvdb
+#Adding entry in fstab reboot mount
+echo "/dev/xvdb       /data   auto    defaults,noatime     0       0" >> /etc/fstab
+
+echo "=========Drive Added===="
+
+#Change to New Drive
+cd /data/
+mkdir -p /data/ebsnew/
+df -h
+lsblk
+cd /data/ebsnew/
+wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py
+ls
+echo "done"
