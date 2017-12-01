a71b7d1ba6593263b90f6e90d1f067092fe6c90b
sivakumarkatam <siva.katam@blazeclan.com>
2017-12-01 17:47:52 +0530

    Update testebs.sh

--- testebs.sh
+++ testebs.sh
@@ -1,22 +1,25 @@
 sudo su -
 echo "starting ebs volume"
+sleep 30s
 lsblk
 df -h
 echo "=========Drive========="
 #Create Mount Directory
 mkdir /newdata
 #Format the drive
-#sudo mkfs -t ext4 /dev/xvdb
+sudo mkfs -t ext4 /dev/xvdb
 #mount
 #mount -L /dev/xvdb /newdata
+fdisk -l
 #Adding entry in fstab reboot mount
 echo "display blkid file"
-#blkid > ne
-#cat ne
-#uid=$(cat ne | grep /dev/xvdb |awk '{ print $2 }' | cut -f2 -d"=" | sed 's/.$//; s/^.//')
-#echo "UUID=$uid /newdata               ext4      defaults     0 0" >> /etc/fstab
-#echo $uid
-#mount /newdata
+sleep 30s
+blkid > ne
+cat ne
+uid=$(cat ne | grep /dev/xvdb |awk '{ print $2 }' | cut -f2 -d"=" | sed 's/.$//; s/^.//')
+echo "UUID=$uid /newdata               ext4      defaults     0 0" >> /etc/fstab
+echo $uid
+mount /newdata
 echo "=========Drive Added===="
 
 #Change to New Drive
